0df99dc2958408735ca02e973f5aa46cb7656d95
pablocorks <pablomrx@gmail.com>
2025-08-07 13:17:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,22 @@ async function enviarFormulario(event) {
   const url = 'https://script.google.com/macros/s/AKfycbzoEj-dg4pdZhhQUXVIatktaAAh5bMMt_li0QUvbSm3b0aPcaSCDF8Lt0v0MnGgR3n5Dg/exec';
 
   try {
-    await fetch(url, {
+    const resposta = await fetch(url, {
       method: 'POST',
       body: JSON.stringify({ nome }),
       headers: {
         'Content-Type': 'application/json'
       }
     });
-    window.location.href = 'index.html';
+
+    const texto = await resposta.text();
+    if (resposta.ok) {
+      window.location.href = 'index.html';
+    } else {
+      alert('Erro do servidor: ' + texto);
+    }
+
   } catch (e) {
-    alert('Erro ao registrar chegada');
+    alert('Erro ao conectar: ' + e.message);
   }
 }
